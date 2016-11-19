4606ebf2d2fc8369863405a7e1a90ee8a81e1226
Hsin Yuan Yeh <iapyeh@gmail.com>
2016-11-19 18:24:15 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -35,13 +35,13 @@ extlinks = {
 # -- Options for HTML output ---------------------------------------------------
 
 html_theme = 'default'
-if not on_rtd:
-    try:
-        import sphinx_rtd_theme
-        html_theme = 'sphinx_rtd_theme'
-        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-    except ImportError:
-        pass
+#if not on_rtd:
+#    try:
+#        import sphinx_rtd_theme
+#        html_theme = 'sphinx_rtd_theme'
+#        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#    except ImportError:
+#        pass
 
 html_static_path = ['static']
 
