c88af87410e761b7bef6b5efc6201ac098936ee6
Hsin Yuan Yeh <iapyeh@gmail.com>
2016-12-01 15:02:26 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -64,10 +64,21 @@ html_use_index = False
 html_show_sourcelink = False
 
 """
+
+def run_apidoc(_):
+    from sphinx.apidoc import main
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+    cur_dir = os.path.abspath(os.path.dirname(__file__))
+    module = '.'
+    output_path = os.path.join(cur_dir, 'source')
+    # main(['-e', '-o', output_path, module, '--force'])
+
 def setup(app):
     # overrides for wide tables in RTD theme
     app.add_stylesheet('theme_overrides.css')   # path relative to _static
-
+    app.connect('builder-inited', run_apidoc)
 """
 
 # -- Options for LaTeX output --------------------------------------------------
