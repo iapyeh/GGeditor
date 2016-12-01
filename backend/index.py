46941e1a1a5c3e0511f01037aefa1448612b3854
DMID Lab/iap <iap@iis.sinica.edu.tw>
2016-12-01 15:50:20 +0800

    add backend/index.py

---
+++ backend/index.py
@@ -0,0 +1,19 @@
+"""
+Welcome to test's documentation!
+================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+"""
